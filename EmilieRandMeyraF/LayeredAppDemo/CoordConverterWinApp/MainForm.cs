25eb0d984b819d1f4d54a2575c4fe6569992f9e2
DESKTOP-7EES3O2\Emilie Rabeau <mie_rabeau@rogers.com>
2018-03-19 12:42:36 -0400

    created the winApp

--- EmilieRandMeyraF/LayeredAppDemo/CoordConverterWinApp/MainForm.cs
+++ EmilieRandMeyraF/LayeredAppDemo/CoordConverterWinApp/MainForm.cs
@@ -7,14 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CoordConvertersBLL;
+
 
 namespace CoordConverterWinApp
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
